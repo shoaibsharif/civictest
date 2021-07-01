cdc27a3f5ed2e0badee69504dbf1e5d53b7dc575
Shoaib Sharif <shoaib19j@gmail.com>
2021-06-30 21:29:32 -0700

    update jsDoc

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,21 +5,33 @@ import { quiz } from "../lib/civic";
 import produce from "immer";
 
 export default function Home() {
+  /**
+   * Return a random value between 0 to max
+   * @param {number} max
+   * @returns {number}
+   */
   const randomInt = (max) => Math.floor(Math.random() * max);
-  const randomArray = useCallback((length, max = 10) => {
+
+  /**
+   * Get a random array with unique value
+   * @param {number} length array length
+   * @param {number} max between 0 and max number
+   * @returns {number[]} an array
+   */
+  const randomArray = (length, max = 10) => {
     const arr = [];
     while (arr.length < length) {
       const randomNumber = randomInt(max);
       if (arr.indexOf(randomNumber) == -1) arr.push(randomNumber);
     }
     return arr;
-  }, []);
+  };
   const [randomQuiz, setRandomQuiz] = useState([
     { question: "", answer: [], revealed: false },
   ]);
   useEffect(() => {
     setRandomQuiz(
-      randomArray(10, 29).map((index) => ({
+      randomArray(10, quiz.length).map((index) => ({
         ...quiz[index],
         revealed: false,
       }))
@@ -44,11 +56,11 @@ export default function Home() {
       <header className="max-w-3xl mx-auto mt-9">
         <h1 className="text-5xl text-center">Civic test</h1>
       </header>
-      <main className="max-w-3xl mx-auto px-5">
+      <main className="max-w-3xl px-5 mx-auto">
         <ul>
           {randomQuiz.map((quz, i) => (
             <li
-              className="border-2 border-gray-400 mt-6 p-10 rounded-md ring ring-opacity-30"
+              className="p-10 mt-6 border-2 border-gray-400 rounded-md ring ring-opacity-30"
               key={`question-${i}`}
             >
               <h2 className="text-2xl md:text-3xl">{quz.question}</h2>
@@ -56,7 +68,7 @@ export default function Home() {
                 <ul>
                   {quz.answer.map((ans) => (
                     <li
-                      className="mt-5 font-belleza text-xl list-disc"
+                      className="mt-5 text-xl list-disc font-belleza"
                       key={`answer-${ans}`}
                     >
                       {ans}
@@ -65,7 +77,7 @@ export default function Home() {
                 </ul>
               ) : (
                 <button
-                  className=" bg-indigo-600 px-3 py-3 rounded mt-3"
+                  className="px-3 py-3 mt-3 bg-indigo-600 rounded "
                   onClick={() => changeRevelState(i)}
                 >
                   Reveal the answer
